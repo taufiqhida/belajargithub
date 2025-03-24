dee913fac312d77e2a752a4f95cab76bc5c5dea5
taufiqhida <taufiqh2003@gmail.com>
2025-03-24 07:40:09 +0700

    rapihin

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Belajar PHP</h1>
+    <p>Selamat datang di <b>Belajar PHP</b></p>
+    <?php
+        echo "Hello World!";
+    ?>
+</body>
+</html>
