f1887f208cc2965ba4bc94be7c0bce90f4611435
taufiqhida <taufiqh2003@gmail.com>
2025-03-24 07:52:37 +0700

    tambah nama taufiq

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Belajar PHP</h1>
-    <p>Selamat datang di <b>Belajar PHP Muhamad Taufiq</b></p>
+    <p>Selamat datang di <b>Belajar PHP F</b></p>
     <p>Muhamad Taufiq</p>
     <?php
         echo "Hello World!";
